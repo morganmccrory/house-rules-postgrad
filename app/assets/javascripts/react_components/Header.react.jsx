657ba899fd8cffa136d8d1ad86a39f69ec18efbc
morganmccrory <morganmccrory@gmail.com>
2015-05-13 16:46:07 -0500

    Update logo

--- app/assets/javascripts/react_components/Header.react.jsx
+++ app/assets/javascripts/react_components/Header.react.jsx
@@ -116,7 +116,7 @@ var Header = React.createClass({
         <section className="top-bar-section">
           {leftNav}
           {rightNav}
-          <div className="top-logo"><a href="/"><img src={"/assets/house-rules.jpg"} /></a></div>
+          <div className="top-logo"><a href="/"><img src="http://i.imgur.com/OjXcIZb.jpg" /></a></div>
         </section>
       </nav>
     );
