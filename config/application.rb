71ed58bfa46f9740e70c03a5dd48f5bfacaa419f
morganmccrory <morganmccrory@gmail.com>
2015-05-15 15:14:34 -0500

    Add favicon to precompile file

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module HouseRules
   class Application < Rails::Application
     # Use the responders controller from the responders gem
     config.app_generators.scaffold_controller :responders_controller
+    config.assets.precompile += ["favicon.ico"]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
